c5344128d1a624bf83c44b5265eddc72c1f63654
Raghad Hanon <raghadhanoon2015@gmail.com>
2024-08-18 23:49:42 +0300

    Reorder files and add gitignore

--- Inventory_Management_System/obj/Debug/net8.0/Inventory_Management_System.AssemblyInfo.cs
+++ Inventory_Management_System/obj/Debug/net8.0/Inventory_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+554bb7070000ed724594f905ebb069d33f74806a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cd0f7cf01ac995cd1b3f080cd3d3435c70638c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
